f115052664663e55a441d10718f3868d21b074d9
Sanskar Pardesi <sanskarpardesi@gmail.com>
2025-02-28 19:59:05 -0500

    Initial Commit

---
+++ BookLibrary/BookLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookLibraryApp.swift
+//  BookLibrary
+//
+//  Created by Sanskar Pardesi on 2025-02-28.
+//
+
+import SwiftUI
+
+@main
+struct BookLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
